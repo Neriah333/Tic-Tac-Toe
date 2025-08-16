40175b13a944d33cef31667e6ec575d5939fd70b
Neriah333 <pheobenyawanda@gmail.com>
2025-08-16 12:09:56 +0300

    Second commit

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,10 @@
+import Board from "./components/Board.jsx";
+
+export default function App() {
+  return (
+    <div className="game">
+      <h1>Tic-Tac-Toe</h1>
+      <Board />
+    </div>
+  );
+}
